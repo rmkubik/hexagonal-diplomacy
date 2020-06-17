b47961d639ea231c0eaf73b6a0c7c16f973f0532
Ryan Kubik <rmkubik@me.com>
2020-06-16 21:28:39 -0700

    Add deploy fns

--- functions/index.js
+++ functions/index.js
@@ -42,22 +42,3 @@ exports.readGame = functions.https.onRequest(async (req, res) => {
   // Send back a message that we've succesfully written the message
   res.json(game);
 });
-
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore
-  .document("/messages/{documentId}")
-  .onCreate((snap, context) => {
-    // Grab the current value of what was written to Cloud Firestore.
-    const original = snap.data().original;
-
-    // Access the parameter `{documentId}` with `context.params`
-    console.log("Uppercasing", context.params.documentId, original);
-
-    const uppercase = original.toUpperCase();
-
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to Cloud Firestore.
-    // Setting an 'uppercase' field in Cloud Firestore document returns a Promise.
-    return snap.ref.set({ uppercase }, { merge: true });
-  });
